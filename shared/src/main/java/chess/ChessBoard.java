9e9e5a0e23815eddacd70b6282423e5e66e35a5b
matthewsama17 <m.3herron@gmail.com>
2024-02-02 19:18:41 -0700

    Made board final

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
  */
 public class ChessBoard {
 
-    private ChessPiece[][] board = new ChessPiece[8][8];
+    private final ChessPiece[][] board = new ChessPiece[8][8];
     public ChessBoard() { }
 
     /**
