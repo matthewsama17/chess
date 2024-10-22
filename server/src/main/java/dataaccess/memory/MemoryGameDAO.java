221fcd8e04e66c75269b739fe8041c429d056d28
Matthew Herron <m.3herron@gmail.com>
2024-10-22 02:23:16 -0600

    Implemented MemoryGameDAO

---
+++ server/src/main/java/dataaccess/memory/MemoryGameDAO.java
@@ -0,0 +1,47 @@
+package dataaccess.memory;
+
+import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import model.GameData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemoryGameDAO implements GameDAO {
+    static List<GameData> dataset = new ArrayList<>();
+
+    @Override
+    public void createGame(GameData gameData) {
+        dataset.add(gameData);
+    }
+
+    @Override
+    public GameData getGame(int gameID) {
+        for(GameData gameData : dataset) {
+            if(gameData.gameID() == gameID) {
+                return gameData;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<GameData> listGames() {
+        return dataset;
+    }
+
+    @Override
+    public void updateGame(GameData gameData) throws DataAccessException {
+        GameData oldGameData = getGame(gameData.gameID());
+        if(oldGameData == null) {
+            throw new DataAccessException("updateGame tried to update a game that does not exist");
+        }
+        int index = dataset.indexOf(oldGameData);
+        dataset.set(index, gameData);
+    }
+
+    @Override
+    public void clear() {
+        dataset = new ArrayList<>();
+    }
+}
