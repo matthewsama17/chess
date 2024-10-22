95643767e00338ed0708c47ece4adcda6014c73f
Matthew Herron <m.3herron@gmail.com>
2024-10-22 16:53:21 -0600

    Finished writing the service tests.

--- server/src/test/java/service/ServiceTests.java
+++ server/src/test/java/service/ServiceTests.java
@@ -1,5 +1,6 @@
 package service;
 
+import chess.ChessGame;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -73,33 +74,59 @@ public class ServiceTests {
     }
 
     @Test
-    public void testListGames() {
-
-    }
-
-    @Test
-    public void testListGamesFail() {
-
-    }
-
-    @Test
     public void testCreateGame() {
+        String authToken = initializeUser();
+        CreateGameRequest createGameRequest = new CreateGameRequest("Matthew's Game");
 
+        Assertions.assertDoesNotThrow(() -> CreateGameService.createGame(authToken, createGameRequest));
     }
 
     @Test
     public void testCreateGameFail() {
+        String authToken = "aDumbAuthToken";
+        CreateGameRequest createGameRequest = new CreateGameRequest("Matthew's Game");
 
+        Assertions.assertThrows(ServiceException.class, () -> CreateGameService.createGame(authToken, createGameRequest));
+    }
+
+    @Test
+    public void testListGames() {
+        String authToken = initializeUser();
+        CreateGameRequest createGameRequest = new CreateGameRequest("Matthew's Game");
+        CreateGameService.createGame(createGameRequest);
+
+        Assertions.assertEquals(ListGamesService.listGames(authToken).games()[0].gameName(), createGameRequest.gameName());
+    }
+
+    @Test
+    public void testListGamesFail() {
+        String authToken = "aDumbAuthToken";
+
+        Assertions.assertThrows(ServiceException.class, ListGamesService.listGames(authToken));
     }
 
     @Test
     public void testJoinGame() {
+        String authToken = initializeUser();
+        CreateGameRequest createGameRequest = new CreateGameRequest("Matthew's Game");
+        CreateGameResult createGameResult = CreateGameService.createGame(createGameRequest);
 
+        JoinGameRequest joinGameRequest = new JoinGameRequest(ChessGame.TeamColor.WHITE, createGameResult.gameID());
+        JoinGameService.joinGame(authToken, joinGameRequest);
+
+        Assertions.assertEquals("Matthew", ListGamesService.listGames(authToken).games()[0].whiteUsername());
     }
 
     @Test
     public void testJoinGameFail() {
+        String authToken = initializeUser();
+        CreateGameRequest createGameRequest = new CreateGameRequest("Matthew's Game");
+        CreateGameResult createGameResult = CreateGameService.createGame(createGameRequest);
 
+        JoinGameRequest joinGameRequest = new JoinGameRequest(ChessGame.TeamColor.WHITE, createGameResult.gameID());
+        JoinGameService.joinGame(authToken, joinGameRequest);
+
+        Assertions.assertThrows(ServiceException.class, () -> JoinGameService.joinGame(authToken, joinGameRequest));
     }
 
     private String initializeUser() {
