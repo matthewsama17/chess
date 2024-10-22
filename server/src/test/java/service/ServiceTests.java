25d886ee91e3f6957b484a37b5caa5980104e870
Matthew Herron <m.3herron@gmail.com>
2024-10-22 03:13:17 -0600

    Added ServiceTests

---
+++ server/src/test/java/service/ServiceTests.java
@@ -0,0 +1,78 @@
+package java.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ServiceTests {
+
+    @AfterEach
+    public void clearForTests() {
+
+    }
+
+    @Test
+    public void testClear() {
+
+    }
+
+    @Test
+    public void testRegister() {
+
+    }
+
+    @Test
+    public void testRegisterFail() {
+
+    }
+
+    @Test
+    public void testLogin() {
+
+    }
+
+    @Test
+    public void testLoginFail() {
+
+    }
+
+    @Test
+    public void testLogout() {
+
+    }
+
+    @Test
+    public void testLogoutFail() {
+
+    }
+
+    @Test
+    public void testListGames() {
+
+    }
+
+    @Test
+    public void testListGamesFail() {
+
+    }
+
+    @Test
+    public void testCreateGame() {
+
+    }
+
+    @Test
+    public void testCreateGameFail() {
+
+    }
+
+    @Test
+    public void testJoinGame() {
+
+    }
+
+    @Test
+    public void testJoinGameFail() {
+
+    }
+}
