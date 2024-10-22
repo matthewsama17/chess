b0651c88a9e77c4caa85500cf34ca2c921c98960
Matthew Herron <m.3herron@gmail.com>
2024-10-22 01:18:11 -0600

    Made sure Gson works (it does)

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,10 +1,19 @@
 import chess.*;
+import com.google.gson.Gson;
+import model.UserData;
 import server.Server;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+
+        Gson gson = new Gson();
+        UserData userData = new UserData("Matthew", "goodPassWord", "mgh57@byu.edu");
+        String jsonString = gson.toJson(userData);
+        System.out.println(jsonString);
+        UserData jsonData = gson.fromJson(jsonString, UserData.class);
+        System.out.println("username is " + jsonData.username() + ", password is " + jsonData.password());
 
         Server server = new Server();
         server.run(8080);
