aa2b334e9d469946267561e84e83d0e17d5c9bc2
Matthew Herron <m.3herron@gmail.com>
2024-10-22 19:13:02 -0600

    Implemented joinGame

--- server/src/main/java/service/JoinGameService.java
+++ server/src/main/java/service/JoinGameService.java
@@ -1,11 +1,39 @@
 package service;
 
+import chess.ChessGame;
+import dataaccess.DataAccessException;
+import model.AuthData;
+import model.GameData;
 import request.JoinGameRequest;
 
 public class JoinGameService extends Service {
 
-    public void joinGame(String authToken, JoinGameRequest joinGameRequest) {
+    public void joinGame(String authToken, JoinGameRequest joinGameRequest) throws ServiceException {
+        AuthData authData = checkAuthorization(authToken);
+        String username = authData.username();
 
+        GameData gameData = gameDAO.getGame(joinGameRequest.gameID());
+        if(gameData == null) {
+            throw new ServiceException("Error: bad request", 400);
+        }
+        else if(joinGameRequest.playerColor() == ChessGame.TeamColor.WHITE
+                && gameData.whiteUsername() == null) {
+            gameData = new GameData(gameData.gameID(), username, gameData.blackUsername(), gameData.gameName(), gameData.game());
+        }
+        else if(joinGameRequest.playerColor() == ChessGame.TeamColor.BLACK
+                && gameData.blackUsername() == null) {
+            gameData = new GameData(gameData.gameID(), gameData.whiteUsername(), username, gameData.gameName(), gameData.game());
+        }
+        else {
+            throw new ServiceException("Error: already taken", 403);
+        }
+
+        try {
+            gameDAO.updateGame(gameData);
+        }
+        catch(DataAccessException ex) {
+            throw new ServiceException("Error: game update failed", 500);
+        }
     }
 
 }
