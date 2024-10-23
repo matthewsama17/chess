42783d79bcb03c0e788497bfaf06aa4b4f6cbc74
Matthew Herron <m.3herron@gmail.com>
2024-10-22 18:23:27 -0600

    Implemented register

--- server/src/main/java/service/RegisterService.java
+++ server/src/main/java/service/RegisterService.java
@@ -1,11 +1,24 @@
 package service;
 
+import model.AuthData;
+import model.UserData;
 import request.RegisterRequest;
 import result.LoginResult;
 
 public class RegisterService extends Service {
 
     public LoginResult register(RegisterRequest registerRequest) throws ServiceException {
-        return null;
+        if(userDAO.getUser(registerRequest.username()) != null) {
+            throw new ServiceException("Error: already taken", 403);
+        }
+
+        UserData userData = new UserData(registerRequest.username(), registerRequest.password(), registerRequest.email());
+        userDAO.createUser(userData);
+
+        String authToken = generateAuthToken();
+        AuthData authData = new AuthData(authToken, registerRequest.username());
+        authDAO.createAuth(authData);
+
+        return new LoginResult(registerRequest.username(), authToken);
     }
 }
--- server/src/main/java/service/Service.java
+++ server/src/main/java/service/Service.java
@@ -2,9 +2,14 @@ package service;
 
 import dataaccess.*;
 import dataaccess.memory.*;
+import java.util.UUID;
 
 public class Service {
     AuthDAO authDAO = new MemoryAuthDAO();
     GameDAO gameDAO = new MemoryGameDAO();
     UserDAO userDAO = new MemoryUserDAO();
+
+    public String generateAuthToken() {
+        return UUID.randomUUID().toString();
+    }
 }
