196c56df79cbd8133f18dba83d77efede5627fb9
Matthew Herron <m.3herron@gmail.com>
2024-10-22 20:52:32 -0600

    Added HTTP endpoints to Server class

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,5 +1,6 @@
 package server;
 
+import server.handler.*;
 import spark.*;
 
 public class Server {
@@ -10,6 +11,13 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.delete("/db", ClearHandler::handleRequest);
+        Spark.post("/user", RegisterHandler::handleRequest);
+        Spark.post("/session", LoginHandler::handleRequest);
+        Spark.delete("/session", LogoutHandler::handleRequest);
+        Spark.get("/game", ListGamesHandler::handleRequest);
+        Spark.post("/game", CreateGameHandler::handleRequest);
+        Spark.put("/game", JoinGameHandler::handleRequest);
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
