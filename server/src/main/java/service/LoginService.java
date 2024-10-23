e3156c5e1384b3c2ed7191041513d66b4d2b72f4
Matthew Herron <m.3herron@gmail.com>
2024-10-22 18:36:46 -0600

    Implemented login

--- server/src/main/java/service/LoginService.java
+++ server/src/main/java/service/LoginService.java
@@ -1,11 +1,23 @@
 package service;
 
+import model.AuthData;
+import model.UserData;
 import request.LoginRequest;
 import result.LoginResult;
 
 public class LoginService extends Service {
 
     public LoginResult login(LoginRequest loginRequest) throws ServiceException {
-        return null;
+        UserData userData = userDAO.getUser(loginRequest.username());
+        if(userData == null
+                || !userData.password().equals(loginRequest.password())) {
+            throw new ServiceException("Error: unauthorized",  401);
+        }
+
+        String authToken = generateAuthToken();
+        AuthData authData = new AuthData(authToken, loginRequest.username());
+        authDAO.createAuth(authData);
+
+        return new LoginResult(loginRequest.username(), authToken);
     }
 }
