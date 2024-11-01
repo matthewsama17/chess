bc1961fc8f4f2d199d476ae162cf51f3d2ba00c6
Matthew Herron <m.3herron@gmail.com>
2024-10-31 22:17:26 -0600

    Implemented half of SQLGameDAO methods

--- server/src/main/java/dataaccess/sql/SQLGameDAO.java
+++ server/src/main/java/dataaccess/sql/SQLGameDAO.java
@@ -1,11 +1,18 @@
 package dataaccess.sql;
 
+import chess.ChessGame;
 import dataaccess.DataAccessException;
 import dataaccess.DatabaseManager;
 import dataaccess.GameDAO;
 import model.GameData;
 
+import com.google.gson.Gson;
+
+import java.sql.*;
+
 public class SQLGameDAO implements GameDAO {
+
+    private Gson gson = new Gson();
 
     static {
         try {
@@ -15,14 +22,87 @@ public class SQLGameDAO implements GameDAO {
         }
     }
 
+    static {
+        String SQLCommand = """
+                CREATE TABLE IF NOT EXISTS game (
+                gameID INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                whiteUsername VARCHAR(20),
+                blackUsername VARCHAR(20),
+                gameName VARCHAR(20) NOT NULL,
+                gameJson VARCHAR(2048) NOT NULL
+                )
+                """;
+        try(Connection conn = DatabaseManager.getConnection()) {
+            try(PreparedStatement ps = conn.prepareStatement(SQLCommand)) {
+                ps.executeUpdate();
+            }
+        }
+        catch(DataAccessException | SQLException ex) {
+            throw new RuntimeException(ex.getMessage());
+        }
+    }
+
     @Override
     public int createGame(GameData gameData) {
-        return 0;
+        String SQLCommand = """
+                INSERT INTO game (whiteUsername, blackUsername, gameName, gameJson) VALUES (?, ?, ?, ?)
+                """;
+        int gameID = 0;
+
+        try(Connection conn = DatabaseManager.getConnection()) {
+            try(PreparedStatement ps = conn.prepareStatement(SQLCommand, Statement.RETURN_GENERATED_KEYS)) {
+                ps.setString(1, gameData.whiteUsername());
+                ps.setString(2, gameData.blackUsername());
+                ps.setString(3, gameData.gameName());
+                String gameJson = gson.toJson(gameData.game());
+                ps.setString(4, gameJson);
+
+                if(ps.executeUpdate() == 1) {
+                    try (ResultSet rs = ps.getGeneratedKeys()) {
+                        rs.next();
+                        gameID =  rs.getInt(1);
+                    }
+                }
+            }
+        }
+        catch(DataAccessException | SQLException ex) {
+            throw new RuntimeException(ex.getMessage());
+        }
+        return gameID;
     }
 
     @Override
     public GameData getGame(int gameID) {
-        return null;
+        String SQLCommand = """
+                SELECT whiteUsername, blackUsername, gameName, gameJson
+                FROM game
+                WHERE gameID = ?
+                """;
+        GameData gameData = null;
+
+        try(Connection conn = DatabaseManager.getConnection()) {
+            try(PreparedStatement ps = conn.prepareStatement(SQLCommand)) {
+                ps.setInt(1, gameID);
+
+                try(ResultSet rs = ps.executeQuery()) {
+                    if(rs.next()) {
+                        String whiteUsername = rs.getString(1);
+                        String blackUsername = rs.getString(2);
+                        String gameName = rs.getString(3);
+                        String gameJson = rs.getString(4);
+
+                        ChessGame game = gson.fromJson(gameJson, ChessGame.class);
+
+                        gameData = new GameData(gameID, whiteUsername, blackUsername, gameName, game);
+                    }
+                }
+            }
+        }
+        catch(DataAccessException | SQLException ex) {
+            throw new RuntimeException(ex.getMessage());
+        }
+
+        return gameData;
     }
 
     @Override
@@ -37,6 +117,19 @@ public class SQLGameDAO implements GameDAO {
 
     @Override
     public void clear() {
+        String SQLCommand = "DELETE FROM game";
+        String SQLCommand2 = "ALTER TABLE game AUTO_INCREMENT = 1";
 
+        try(Connection conn = DatabaseManager.getConnection()) {
+            try(PreparedStatement ps = conn.prepareStatement(SQLCommand)) {
+                ps.executeUpdate();
+            }
+            try(PreparedStatement ps = conn.prepareStatement(SQLCommand2)) {
+                ps.executeUpdate();
+            }
+        }
+        catch(DataAccessException | SQLException ex) {
+            throw new RuntimeException(ex.getMessage());
+        }
     }
 }
