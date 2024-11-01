92a920bdb4ee60eb7df77f3a66134733a2a00689
Matthew Herron <m.3herron@gmail.com>
2024-10-31 21:31:04 -0600

    Not sure where that even came from

--- server/src/main/java/dataaccess/sql/SQLUserDAO.java
+++ server/src/main/java/dataaccess/sql/SQLUserDAO.java
@@ -5,7 +5,6 @@ import dataaccess.DatabaseManager;
 import dataaccess.UserDAO;
 import model.UserData;
 
-import javax.xml.crypto.Data;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
