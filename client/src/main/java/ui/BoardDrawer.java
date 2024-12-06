8e8faee4b8242dad3ed7659e1362c6698253c63c
Matthew Herron <m.3herron@gmail.com>
2024-12-05 18:25:55 -0700

    Added move drawing

--- client/src/main/java/ui/BoardDrawer.java
+++ client/src/main/java/ui/BoardDrawer.java
@@ -6,6 +6,8 @@ import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 
 import static ui.EscapeSequences.*;
+import java.util.Collection;
+import java.util.HashSet;
 
 public class BoardDrawer {
     private static String[] letters = {" A ", " B ", " C ", " D ", " E ", " F ", " G ", " H "};
@@ -16,18 +18,43 @@ public class BoardDrawer {
 
         draw(board);
         System.out.println();
+
         board.movePiece(new ChessMove(new ChessPosition(2, 5), new ChessPosition(4, 5), null));
         draw(board, ChessGame.TeamColor.BLACK);
+        System.out.println();
+
+        board.movePiece(new ChessMove(new ChessPosition(7, 5), new ChessPosition(5, 5), null));
+        board.movePiece(new ChessMove(new ChessPosition(1, 4), new ChessPosition(3, 6), null));
+        board.movePiece(new ChessMove(new ChessPosition(8, 4), new ChessPosition(6, 6), null));
+        ChessPosition startPosition = new ChessPosition(3, 6);
+        drawMoves(board, ChessGame.TeamColor.WHITE, board.getPieceMoves(startPosition), startPosition);
+
+        startPosition = new ChessPosition(6, 6);
+        drawMoves(board, ChessGame.TeamColor.BLACK, board.getPieceMoves(startPosition), startPosition);
     }
 
-    public static void draw(ChessBoard board) {
-        draw(board, ChessGame.TeamColor.WHITE);
+    public static void drawMoves(ChessBoard board, ChessGame.TeamColor color, Collection<ChessMove> moves, ChessPosition startPosition) {
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+
+        Collection<ChessPosition> endPositions = new HashSet<>();
+        if(moves != null) {
+            for (ChessMove move : moves) {
+                endPositions.add(move.getEndPosition());
+            }
+        }
+
+        drawBoard(out, board, color, endPositions, startPosition);
     }
 
     public static void draw(ChessBoard board, ChessGame.TeamColor color) {
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+        Collection<ChessPosition> endPositions = new HashSet<>();
 
-        drawBoard(out, board, color);
+        drawBoard(out, board, color, endPositions, null);
+    }
+
+    public static void draw(ChessBoard board) {
+        draw(board, ChessGame.TeamColor.WHITE);
     }
 
     public static void draw(ChessGame.TeamColor color) {
@@ -40,7 +67,7 @@ public class BoardDrawer {
         draw(ChessGame.TeamColor.WHITE);
     }
 
-    private static void drawBoard(PrintStream out, ChessBoard board, ChessGame.TeamColor color) {
+    private static void drawBoard(PrintStream out, ChessBoard board, ChessGame.TeamColor color, Collection<ChessPosition> endPositions, ChessPosition startPosition) {
         drawBorder(out, color, true);
         for(int i = 1; i <= 8; i++) {
             int row = i;
@@ -48,7 +75,7 @@ public class BoardDrawer {
                 row = 9-i;
             }
             drawSideBorder(out, row);
-            drawBoardRow(out, board, row, color);
+            drawBoardRow(out, board, row, color, endPositions, startPosition);
             drawSideBorder(out, row);
             finishLine(out);
         }
@@ -97,28 +124,36 @@ public class BoardDrawer {
         out.print(" ");
     }
 
-    private static void drawBoardRow(PrintStream out, ChessBoard board, int row, ChessGame.TeamColor color) {
+    private static void drawBoardRow(PrintStream out, ChessBoard board, int row, ChessGame.TeamColor color, Collection<ChessPosition> endPositions, ChessPosition startPosition) {
         for(int i = 1; i <= 8; i++) {
             int col = i;
             if(color == ChessGame.TeamColor.BLACK) {
                 col = 9-i;
             }
 
-            setSquareColor(out, row, col);
-            drawPiece(out, board, row, col);
+            setSquareColor(out, row, col, endPositions);
+            drawPiece(out, board, row, col, startPosition);
         }
     }
 
-    private static void setSquareColor(PrintStream out, int row, int col) {
+    private static void setSquareColor(PrintStream out, int row, int col, Collection<ChessPosition> endPositions) {
         if(((row + col) % 2) == 0) {
             out.print(SET_BG_COLOR_BLACK);
+            if(endPositions.contains(new ChessPosition(row, col))) {
+                out.print(SET_BG_COLOR_DARK_YELLOW);
+            }
         }
         else {
             out.print(SET_BG_COLOR_WHITE);
+            if(endPositions.contains(new ChessPosition(row, col))) {
+                out.print(SET_BG_COLOR_YELLOW);
+            }
         }
+
+
     }
 
-    private static void drawPiece(PrintStream out, ChessBoard board, int row, int col) {
+    private static void drawPiece(PrintStream out, ChessBoard board, int row, int col, ChessPosition startPosition) {
         ChessPiece piece = board.getPiece(new ChessPosition(row, col));
 
         String pieceString = null;
@@ -127,6 +162,11 @@ public class BoardDrawer {
         }
         else if(piece.getTeamColor() == ChessGame.TeamColor.BLACK) {
             out.print(SET_TEXT_COLOR_MAGENTA);
+
+            if(new ChessPosition(row, col).equals(startPosition)) {
+                out.print(SET_TEXT_COLOR_BLACK);
+                out.print(SET_BG_COLOR_MAGENTA);
+            }
 
             if(piece.getPieceType() == ChessPiece.PieceType.PAWN) {
                 pieceString = BLACK_PAWN;
@@ -149,6 +189,11 @@ public class BoardDrawer {
         }
         else {
             out.print(SET_TEXT_COLOR_GREEN);
+
+            if(new ChessPosition(row, col).equals(startPosition)) {
+                out.print(SET_TEXT_COLOR_BLACK);
+                out.print(SET_BG_COLOR_GREEN);
+            }
 
             if(piece.getPieceType() == ChessPiece.PieceType.PAWN) {
                 pieceString = WHITE_PAWN;
