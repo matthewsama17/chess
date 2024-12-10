392c9a659cbb6f38f7758c632da907baa0586f32
Matthew Herron <m.3herron@gmail.com>
2024-12-09 18:39:23 -0700

    Added some WebSocket endpoints

--- server/src/main/java/websocket/ConnectionManager.java
+++ server/src/main/java/websocket/ConnectionManager.java
@@ -11,24 +11,20 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ConnectionManager {
     public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
 
-    public void add(String authToken, Session session) {
-        var connection = new Connection(authToken, 0, session);
+    public void add(String authToken, int gameID,  Session session) {
+        var connection = new Connection(authToken, gameID, session);
         connections.put(authToken, connection);
-    }
-
-    public void updateGameID(String authToken, int gameID) {
-        connections.get(authToken).gameID = gameID;
     }
 
     public void remove(String authToken) {
         connections.remove(authToken);
     }
 
-    public void broadcast(String excludeAuthToken, ServerMessage serverMessage) throws IOException {
+    public void broadcast(int gameID, String excludeAuthToken, ServerMessage serverMessage) throws IOException {
         var removeList = new ArrayList<Connection>();
         for (var c : connections.values()) {
             if (c.session.isOpen()) {
-                if (!c.authToken.equals(excludeAuthToken)) {
+                if (!c.authToken.equals(excludeAuthToken) && c.gameID == gameID) {
                     c.send(new Gson().toJson(serverMessage));
                 }
             } else {
--- server/src/main/java/websocket/WebSocketHandler.java
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -1,10 +1,12 @@
 package websocket;
 
+import chess.ChessGame;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonDeserializer;
 import dataaccess.*;
 import dataaccess.sql.*;
+import model.*;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
@@ -15,6 +17,9 @@ import java.io.IOException;
 
 @WebSocket
 public class WebSocketHandler {
+    private AuthDAO authDAO = new SQLAuthDAO();
+    private GameDAO gameDAO = new SQLGameDAO();
+    private UserDAO userDAO = new SQLUserDAO();
 
     private final ConnectionManager connections = new ConnectionManager();
     private Gson gson = new GsonBuilder()
@@ -37,13 +42,156 @@ public class WebSocketHandler {
     @OnWebSocketMessage
     public void onMessage(Session session, String message) throws IOException {
         UserGameCommand command = gson.fromJson(message, UserGameCommand.class);
-        connections.add(command.getAuthToken(), session);
 
-        ServerMessage serverMessage = new NotificationMessage(gson.toJson(command));
-        if(command.getCommandType() == UserGameCommand.CommandType.MAKE_MOVE) {
-            MakeMoveCommand makeMoveCommand = (MakeMoveCommand) command;
-            serverMessage = new NotificationMessage("This is a Move: " + gson.toJson(makeMoveCommand));
+        String username = null;
+        try {
+            AuthData authData = authDAO.getAuth(command.getAuthToken());
+            if(authData == null) {
+                throw new Exception();
+            }
+            username = authData.username();
         }
-        connections.broadcast(command.getAuthToken(), serverMessage);
+        catch (Exception ex) {
+            sendError(session, "ERROR: Your connection has timed out.");
+        }
+
+        GameData gameData = null;
+        try {
+            gameData = gameDAO.getGame(command.getGameID());
+            if(gameData == null) {
+                throw new Exception();
+            }
+        }
+        catch (Exception ex) {
+            sendError(session, "ERROR: Game could not be found.");
+        }
+
+        UserGameCommand.CommandType type = command.getCommandType();
+
+        if(type == UserGameCommand.CommandType.CONNECT) {
+            handleConnect(session, command.getAuthToken(), username, command.getGameID(), gameData);
+        }
+        else if(type == UserGameCommand.CommandType.MAKE_MOVE) {
+
+        }
+        else if(type == UserGameCommand.CommandType.RESIGN) {
+            handleResign(session, command.getAuthToken(), username, command.getGameID(), gameData);
+        }
+        else if(type == UserGameCommand.CommandType.LEAVE) {
+            handleLeave(session, command.getAuthToken(), username, command.getGameID(), gameData);
+        }
+    }
+
+    private void handleConnect(Session session, String authToken, String username, int gameID, GameData gameData) {
+        connections.add(authToken, gameID, session);
+        String notificationString = username + " has joined the game as ";
+
+        if(username.equals(gameData.whiteUsername())) {
+            notificationString += "White";
+        }
+        else if(username.equals(gameData.blackUsername())) {
+            notificationString += "Black";
+        }
+        else {
+            notificationString += "an observer";
+        }
+        broadcastNotification(gameID, authToken, notificationString);
+
+        sendLoadGame(session, gameData.game());
+    }
+
+    private void handleResign(Session session, String authToken, String username, int gameID, GameData gameData) {
+        ChessGame.TeamColor color = null;
+
+        if(username.equals(gameData.whiteUsername())) {
+            color = ChessGame.TeamColor.WHITE;
+        }
+        else if(username.equals(gameData.blackUsername())) {
+            color = ChessGame.TeamColor.BLACK;
+        }
+        else {
+            sendError(session, "ERROR: You can't resign, as you aren't a player");
+            return;
+        }
+
+        if(gameData.resigned() != null) {
+            sendError(session, "ERROR: The game is already over");
+            return;
+        }
+
+        gameData = new GameData(gameData.gameID(), gameData.whiteUsername(), gameData.blackUsername(), gameData.gameName(), gameData.game(), color);
+        try {
+            gameDAO.updateGame(gameData);
+        }
+        catch(Exception ex) {
+            sendError(session, "ERROR: The game could not be resigned");
+        }
+
+        String notificationString = username + " resigned";
+        broadcastNotification(gameID, authToken, notificationString);
+    }
+
+    private void handleLeave(Session session, String authToken, String username, int gameID, GameData gameData) {
+        connections.remove(authToken);
+
+        if(username.equals(gameData.whiteUsername())) {
+            gameData = new GameData(gameData.gameID(), null, gameData.whiteUsername(), gameData.gameName(), gameData.game(), gameData.resigned());
+        }
+        else if(username.equals(gameData.blackUsername())) {
+            gameData = new GameData(gameData.gameID(), gameData.whiteUsername(), null, gameData.gameName(), gameData.game(), gameData.resigned());
+        }
+        else {
+            return;
+        }
+
+        try {
+            gameDAO.updateGame(gameData);
+        }
+        catch(Exception ex) {
+            sendError(session, "ERROR: The game could not be left");
+        }
+
+        String notificationString = username + " left";
+        broadcastNotification(gameID, authToken, notificationString);
+    }
+
+    private void sendNotification(Session session, String string) {
+        NotificationMessage notificationMessage = new NotificationMessage(string);
+        try {
+            session.getRemote().sendString(gson.toJson(notificationMessage));
+        }
+        catch(Exception ignored) { }
+    }
+
+    private void sendError(Session session, String errorString) {
+        ErrorMessage errorMessage = new ErrorMessage(errorString);
+        try {
+            session.getRemote().sendString(gson.toJson(errorMessage));
+        }
+        catch(Exception ignored) { }
+    }
+
+    private void sendLoadGame(Session session, ChessGame game) {
+        LoadGameMessage loadGameMessage = new LoadGameMessage(game);
+        try {
+            session.getRemote().sendString(gson.toJson(loadGameMessage));
+        }
+        catch(Exception ignored) { }
+    }
+
+    private void broadcastNotification(int gameID, String excludeAuthToken, String string) {
+        NotificationMessage notificationMessage = new NotificationMessage(string);
+        try {
+            connections.broadcast(gameID, excludeAuthToken, notificationMessage);
+        }
+        catch(Exception ignored) { }
+    }
+
+    private void broadcastLoadGame(int gameID, String excludeAuthToken, ChessGame game) {
+        LoadGameMessage loadGameMessage = new LoadGameMessage(game);
+        try {
+            connections.broadcast(gameID, excludeAuthToken, loadGameMessage);
+        }
+        catch(Exception ignored) { }
     }
 }
